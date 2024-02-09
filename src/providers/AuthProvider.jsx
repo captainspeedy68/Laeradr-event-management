9a3627520cfeb3c88e16a374052b53e210cb25d7
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-02-09 23:38:37 +0600

    creating context api

---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,18 @@
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import React, { useState } from 'react';
+import { createContext } from 'react';
+import auth from '../firebase/firebase.config';
+export const AuthContext = createContext(null);
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+    const authInfo = {user , createUser}
+    return (
+        <AuthContext.Provider value={authInfo}>
+         {children}   
+        </AuthContext.Provider>
+    );
+};
+export default AuthProvider;
