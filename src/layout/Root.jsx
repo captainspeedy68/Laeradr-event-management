0447db860ee9cfd4a8f9cdecb38e9d5599ed3d0f
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-02-10 23:16:52 +0600

    changing some stuff for the react toasts

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import Footer from '../shared/Footer/Footer';
 import Navbar from '../shared/Navbar/Navbar';
 import BackgroundImage from './BackgroundImage';
@@ -11,6 +12,7 @@ const Root = () => {
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer className = "max-w-none"></Footer>
+            {/* <ToastContainer autoClose={1000}></ToastContainer> */}
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,15 @@ import {
 } from "react-router-dom";
 import router from './routes/Routes.jsx';
 import AuthProvider from './providers/AuthProvider.jsx';
+import { ToastContainer } from 'react-toastify';
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  
     <AuthProvider>
+      <React.StrictMode>
       <RouterProvider router={router}></RouterProvider>
+      <ToastContainer autoClose={1000}></ToastContainer>
+      </React.StrictMode>
     </AuthProvider>
-
-  </React.StrictMode>,
+    
+  
 )
