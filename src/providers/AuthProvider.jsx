044cc54fe5efcb3d596802c181690c720f3f6746
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-02-10 23:15:53 +0600

    providing logout and other context auth providers

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,17 +1,32 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import React, { useState } from 'react';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
+import React, { useEffect, useState } from 'react';
 import { createContext } from 'react';
 import auth from '../firebase/firebase.config';
 export const AuthContext = createContext(null);
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const createUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password);
     }
-    const authInfo = {user , createUser}
+    const logInUser = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+    const logout = () => {
+        return signOut(auth);
+    }
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log("user in the auth state changed", currentUser)
+            setUser(currentUser);
+        });
+        return () => {
+            unsubscribe()
+        }
+    }, [])
+    const authInfo = { user, createUser, logInUser, logout }
     return (
         <AuthContext.Provider value={authInfo}>
-         {children}   
+            {children}
         </AuthContext.Provider>
     );
 };
