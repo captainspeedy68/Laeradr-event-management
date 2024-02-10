4dcd6f39e483d7969acc2bf966e620467813a614
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-02-10 23:16:19 +0600

    useing user and logout context in navbar

--- src/shared/Navbar/Navbar.jsx
+++ src/shared/Navbar/Navbar.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
+import { useContext } from 'react';
 import { useState } from 'react';
 import { Link, NavLink, useLocation } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { AuthContext } from '../../providers/AuthProvider';
 
 const Navbar = () => {
+    const { user, logout } = useContext(AuthContext);
+    // console.log("from navbar", user)
     const location = useLocation();
     const [isBannerVisible, setIsBannerVisible] = useState(false);
     const [clicked, setClicked] = useState(false);
-
+    // const notify = toast("Logged Out!");
     const handleClick = () => {
         setClicked(!clicked);
         setTimeout(() => {
             setClicked(false);
         }, 75);
     };
+    const handleLogout = () => {
+        logout()
+            .then(result => {
+                console.log(result);
+            })
+            .catch(error => {
+                console.log(error.message);
+                // toast(error.message)
+            });
+        // notify();
+        // 
+    }
 
     const links = <>
         <li onClick={() => setIsBannerVisible(true)}><NavLink className={`bg-white active-link btn min-h-4 h-8 mx-1 border-none shadow-none hover:bg-gradient-to-r from-[#ff530a] from-20% via-[#FF7418] via-60% to-[#FFA328] to-95% hover:text-white ${location.pathname === "/" ? "bg-gradient-to-r from-[#ff530a] from-20% via-[#FF7418] via-60% to-[#FFA328] to-95% text-white " : ""}`} to={"/"} >
@@ -46,20 +64,24 @@ const Navbar = () => {
                 </ul>
             </div>
             <div className="navbar-end">
-
-                <section className={`relative hover:cursor-pointer ${clicked ? 'scale-105' : ''} transition-transform duration-75 overflow-hidden bg-gradient-to-r from-[#ff530a] via-[#FF7418] to-[#FFA328] border-2 border-transparent rounded-md px-4 py-2 text-white font-semibold shadow-md mx-auto`} onClick={handleClick}><Link to={"/login"}>
-                    Login
-                    <span className="absolute inset-0 border-2 border-solid border-white rounded-md"></span>
-                </Link>
-                </section>
-
+                {/* add this in the css ${clicked ? 'scale-105' : ''} */}
                 <section className={`relative hover:cursor-pointer ${clicked ? 'scale-105' : ''} transition-transform duration-75 overflow-hidden bg-gradient-to-r from-[#ff530a] via-[#FF7418] to-[#FFA328] border-2 border-transparent rounded-md px-4 py-2 text-white font-semibold shadow-md`} onClick={handleClick}><Link to={"/register"}>
                     Register
                     <span className="absolute inset-0 border-2 border-solid border-white rounded-md"></span>
                 </Link>
                 </section>
+                {
+                    user ? 
+                    <section className={`relative hover:cursor-pointer  ${clicked ? 'scale-105' : ''} transition-transform duration-75 overflow-hidden bg-gradient-to-r from-[#ff530a] via-[#FF7418] to-[#FFA328] border-2 border-transparent rounded-md px-4 py-2 text-white font-semibold shadow-md`} onClick={handleLogout}>Logout</section> : 
+                    <section className={`relative hover:cursor-pointer ${clicked ? 'scale-105' : ''} transition-transform duration-75 overflow-hidden bg-gradient-to-r from-[#ff530a] via-[#FF7418] to-[#FFA328] border-2 border-transparent rounded-md px-4 py-2 text-white font-semibold shadow-md`} onClick={handleClick} ><Link to={"/login"}>
+                        Login
+                        <span className="absolute inset-0 border-2 border-solid border-white rounded-md"></span>
+                    </Link>
+                    </section>
+                }
 
             </div>
+            {/* <ToastContainer></ToastContainer> */}
         </div>
     );
 };
