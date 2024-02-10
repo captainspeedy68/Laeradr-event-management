cf38351f7bfc71688d0a1c51712b8129149ffe9f
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-02-10 23:15:34 +0600

    register password managment

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -2,27 +2,54 @@ import React from 'react';
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../providers/AuthProvider';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
+import 'react-toastify/dist/ReactToastify.css';
+import { useState } from 'react';
 const Register = () => {
-    const {createUser} = useContext(AuthContext);
+    const { createUser } = useContext(AuthContext);
+    const [errorMessage, setErrorMessage] = useState(null);
+    const uppercaseRegex = /[A-Z]/;
+    const specialCharRegex = /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/;
+    const notify = () => toast("Registration Completed");
     const handleregister = e => {
         e.preventDefault();
         const form = new FormData(e.currentTarget);
         const password = form.get("password");
         const email = form.get("email");
-        createUser(email, password)
-        .then(result =>{
-            console.log(result.user);
-        })
-        .catch(error => {
-            console.log(error.message);
-        })
+        setErrorMessage("");
+        const hasUppercase = uppercaseRegex.test(password);
+        const hasSpecialChar = specialCharRegex.test(password);
+        if (password.length < 6){
+            setErrorMessage("Password cannot be less than 6 characters");
+            return;
+        }
+        else if (!hasUppercase){
+            setErrorMessage("Password should have at least one uppercase letter");
+            return;
+        }
+        else if (!hasSpecialChar){
+            setErrorMessage("Password should have at least one special character");
+            return;
+        }
+        
+            createUser(email, password)
+                .then(result => {
+                    console.log(result.user);
+                    // toast("Registration Completed");
+                    notify();
+                })
+                .catch(error => {
+                    console.log(error.message);
+                    setErrorMessage(error.message);
+                })
     }
     return (
         <div>
             <div className="hero min-h-screen bg-base-200">
                 <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    
+
                     <form onSubmit={handleregister} className="card-body">
                         <h3 className='text-center font-semibold'>Register</h3>
                         <div className="form-control">
@@ -49,10 +76,17 @@ const Register = () => {
                         <div className="form-control mt-6">
                             <button className="btn bg-gradient-to-r from-[#ff530a] from-20% via-[#FF7418] via-60% to-[#FFA328] to-95% text-white">register</button>
                         </div>
-                    <p className='text-center'>Already Have An Account? <Link className='text-[#ff530a] font-semibold' to={"/login"}>Login</Link></p>
+                        {
+                            errorMessage &&
+                            <p className='text-red-400'>{errorMessage}</p>
+                        }
+                        <p className='text-center'>Already Have An Account? <Link className='text-[#ff530a] font-semibold' to={"/login"}>Login</Link></p>
+
                     </form>
                 </div>
+
             </div>
+            {/* <ToastContainer /> */}
         </div>
     );
 };
