d002d57ca8c6e2e9469226ccdfae284e30566225
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-02-11 16:01:12 +0600

    adding the events page

--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import IndividualEvents from './IndividualEvents';
 
 const Events = () => {
+    const [displayEvents, setDisplayEvents] = useState([]);
+    useEffect(() => {
+        fetch("/events.json")
+        .then(res => res.json())
+        .then(data => setDisplayEvents(data))
+    }, [])
     return (
-        <div>
-            <h1>an event going on here</h1>
+        // here I shall add the events that attendees can join by paying for the tickets
+        <div className='grid grid-cols-3 gap-2'>
+            {
+                displayEvents.map(event => <IndividualEvents key={event.id} event = {event}></IndividualEvents>)
+            }
         </div>
     );
 };
