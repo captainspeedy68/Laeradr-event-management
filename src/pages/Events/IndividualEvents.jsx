483b08eccaec5bea2a65b70966f7a907183e2d86
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-02-21 13:47:30 +0600

    image changes for the details component

--- src/pages/Events/IndividualEvents.jsx
+++ src/pages/Events/IndividualEvents.jsx
@@ -1,30 +1,33 @@
 import React from 'react';
 import { FaClock, FaRegClock } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
+import { Link } from 'react-router-dom';
 
 const IndividualEvents = ({ event }) => {
-    const { name, description, image, time, location } = event;
+    const { name, description, id, image_url, time, location } = event;
     return (
-            <div className="card md:w-96 bg-base-100 shadow-xl rounded-none event-hover hover:border hover:shadow-2xl border-orange-400 max-md:my-3">
-                <figure className="px-5 pt-5">
-                    <img src={image} alt="Shoes" className="rounded-xl" />
-                </figure>
-                <div className="card-body items-center text-center">
+        <div className="card md:w-96 bg-base-100 shadow-xl rounded-none event-hover hover:border hover:shadow-2xl border-orange-400 max-md:my-3">
+            <figure className="px-5 pt-5">
+                <img src={image_url} alt="Shoes" className="rounded-xl" />
+            </figure>
+            <div className="card-body items-center text-center">
+                <div>
+                    <h2 className="card-title">{name}</h2>
                     <div>
-                        <h2 className="card-title">{name}</h2>
-                        <div>
-                            <div className='flex items-center my-1'><div className='bg-[#FFBE31] p-1 h-6 w-6 rounded-full mr-2 flex justify-center items-center'><FaRegClock className=' text-white h-3 w-3' /></div>{time}</div>
-                            <div className='my-1 flex items-center'><div className='bg-[#FFBE31] p-1 h-6 w-6 rounded-full mr-2 flex justify-center items-center'><FaLocationDot className='text-white' /></div>{location}</div>
-                        </div>
-                    </div>
-
-
-                    <p>{description}</p>
-                    <div className="card-actions">
-                        <button className="btn w-72 rounded-full event-btn">Ticket & Details</button>
+                        <div className='flex items-center my-1'><div className='bg-[#FFBE31] p-1 h-6 w-6 rounded-full mr-2 flex justify-center items-center'><FaRegClock className=' text-white h-3 w-3' /></div>{time}</div>
+                        <div className='my-1 flex items-center'><div className='bg-[#FFBE31] p-1 h-6 w-6 rounded-full mr-2 flex justify-center items-center'><FaLocationDot className='text-white' /></div>{location}</div>
                     </div>
                 </div>
+
+
+                <p>{description}</p>
+                <div className="card-actions">
+                    <Link to={`/event/${id}`}>
+                        <button className="btn w-72 rounded-full event-btn">Ticket & Details</button>
+                    </Link>
+                </div>
             </div>
+        </div>
     );
 };
 
