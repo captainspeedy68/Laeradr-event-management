7ea18d111444c2e523a442b9b8cf03b47d320eab
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-02-22 21:39:49 +0600

    adding google login button in login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -6,6 +6,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import { useContext } from 'react';
 import { useState } from 'react';
 import { FaGoogle } from "react-icons/fa";
+import GoogleButton from '../../shared/GoogleButton/GoogleButton';
 const Login = () => {
     const { logInUser, googleSignIn } = useContext(AuthContext);
     const location = useLocation();
@@ -29,7 +30,7 @@ const Login = () => {
                 // console.log(user.displayName);
                 notify();
                 e.target.reset();//to reset the the login form after login is successful
-                navigate( location?.state ? location.state : "/");//using useNavigate hook to navigate to the homepage after navigation
+                navigate(location?.state ? location.state : "/");//using useNavigate hook to navigate to the homepage after navigation
 
             })
             .catch(error => {
@@ -39,61 +40,56 @@ const Login = () => {
             });
 
     }
-    const handleGoogle = () => {
-        googleSignIn()
-            .then(result => {
-                const logged = result.user;
-                console.log(logged);
-            })
-            .catch(error => {
-                console.log(error.message)
-            })
-    }
+    // const handleGoogle = () => {
+    //     googleSignIn()
+    //         .then(result => {
+    //             const logged = result.user;
+    //             notify();
+    //             navigate("/");
+    //             console.log(logged);
+    //         })
+    //         .catch(error => {
+    //             console.log(error.message);
+    //         })
+    // }
     return (
         <div className='my-4'>
+
             <div className="hero my-10">
-                <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <div className="card services shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <form onSubmit={handleLogin} className="card-body">
-                        <h3 className='text-center font-semibold'>Login!</h3>
+                        <h3 className='text-center text-lg font-semibold'>Login!</h3>
+                        {/* <div className='text-center'>
+                            <div onClick={handleGoogle}
+                                className='inline-block px-6 py-3 text-lg font-semibold text-white bg-gradient-to-r from-yellow-400 to-yellow-600 rounded-full shadow-md hover:shadow-lg hover:cursor-pointer'
+                            >
+                                <div className='flex justify-around items-center'>
+                                    <FaGoogle />
+                                    <span className='ml-3'>Login With Google</span>
+                                </div>
+                            </div>
+                        </div> */}
+                        <GoogleButton></GoogleButton>
+                        <div className='text-center'>or</div>
                         <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+                            <input type="email" name='email' placeholder="email" className="input text-black input-bordered rounded-full" required />
                         </div>
                         <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Password</span>
-                            </label>
-                            <input type="password" name='password' placeholder="password" className="input input-bordered" required />
-                            <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                            </label>
+                            <input type="password" name='password' placeholder="password" className="input rounded-full text-black input-bordered" required />
                         </div>
-                        <div className="form-control mt-6">
-                            <button className="btn bg-gradient-to-r from-[#ff530a] from-20% via-[#FF7418] via-60% to-[#FFA328] to-95% text-white">Login</button>
+                        <div className="form-control mt-3">
+                            <button className="btn rounded-full border-none bg-[#ffbe31] text-white font-bold text-lg">Login</button>
                         </div>
                         {
                             errorMessage &&
                             <p className='text-red-400'>{errorMessage}</p>
                         }
-                        <p className='text-center'>Don't Have An Account? Please <Link className='text-[#ff530a] font-semibold' to={"/register"}>Register</Link></p>
+                        <p className='text-center'>Don't Have An Account? Please <Link className='text-[#FAFAFA] font-semibold' to={"/register"}>Register</Link></p>
 
                     </form>
                 </div>
             </div>
             {/* <ToastContainer autoClose={1000}></ToastContainer> */}
-            <div className='text-center'>
-                <div onClick={handleGoogle}
-                    className='inline-block px-6 py-3 text-lg font-semibold text-white bg-gradient-to-r from-yellow-400 to-yellow-600 rounded-full shadow-md hover:shadow-lg hover:cursor-pointer'
-                >
-                    <div className='flex justify-around items-center'>
-                        <FaGoogle />
-                        <span className='ml-3'>Login With Google</span>
-                    </div>
-
-                </div>
-            </div>
 
         </div>
     );
