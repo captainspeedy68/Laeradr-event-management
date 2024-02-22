2a33f59eee218a38a285e8dd893ba111b67efb86
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-02-22 21:38:58 +0600

    Marquee

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,8 @@ import { AuthContext } from '../../providers/AuthProvider';
 import Services from '../Services/Services';
 import Banner from './Banner';
 import Us from './Us';
-
+import Marquee from "react-fast-marquee";
+import Messages from './Messages/Messages';
 const Home = () => {
     const { user } = useContext(AuthContext);
     const [services, setServices] = useState([]);
@@ -17,10 +18,16 @@ const Home = () => {
     return (
         <div className='m-0 p-0'>
             <Banner services={services}></Banner>
-            <div className='md:grid grid-cols-2 mx-auto'>
+            <Marquee speed={80} className='relative top-72 my-10'>
+                <p className='text-md font-medium text-[#878787] mx-5 mt-10'><span className='font-extrabold'>Breaking News: </span>30% Off In June~July For All Tech Events</p>
+            </Marquee>
+            <hr className='relative top-80 my-10 border-1 border-[#fdac2e]' />
+            <div className='lg:grid grid-cols-2 mx-auto my-10'>
                 <Us></Us>
                 <Services services={services}></Services>
             </div>
+            <hr className='border-1 border-[#FDAC2E]' />
+            <Messages className = "mx-auto"></Messages>
         </div>
     );
 };
