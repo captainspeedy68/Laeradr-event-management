9b4a1a57fa6d3bc1b7ec7bae030f80e967bf7a5e
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-02-22 23:12:49 +0600

    favicon

--- src/pages/Home/Messages/Qna.jsx
+++ src/pages/Home/Messages/Qna.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { useState } from 'react';
 
 const Qna = ({ qnas }) => {
-
+    const [show, setShow] = useState(150);
+    const [clicked, setClicked] = useState(false);
+    const handleLess = () => {
+        
+    }
+    const handleClick =() =>{
+        setShow(500)
+    }
     return (
         <div className="card hover:shadow-2xl md:w-auto bg-base-100 shadow-xl hover:bg-[]">
             <div className="card-body">
@@ -10,11 +18,11 @@ const Qna = ({ qnas }) => {
                 {
                     qnas.map(qna => <div key = {qna.id}><div className="p-4 border rounded-md mb-4">
                             <h2 className="text-xl card-title font-bold mb-2">{qna.question}</h2>
-                            <p className="text-gray-700">{qna.answer.slice(0, 150)}...</p>
+                            <p className="text-gray-700">{qna.answer.slice(0, show)}{(show <= 150) && <span>...</span>}</p>
                         </div></div>)
                 }
                 <div className="card-actions justify-end">
-                    <button className="btn nav-clickable text-white">See More</button>
+                    <button className="btn nav-clickable text-white" onClick={handleClick}>See More</button>
                 </div>
             </div>
         </div>
