cb9f8c00c13b6367cc4c774645ce9e790ddbeffb
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-02-23 01:22:30 +0600

    Qna showing state changes

--- src/pages/Home/Messages/Qna.jsx
+++ src/pages/Home/Messages/Qna.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import { useState } from 'react';
 
 const Qna = ({ qnas }) => {
-    const [show, setShow] = useState(150);
-    const [clicked, setClicked] = useState(false);
-    const handleLess = () => {
-        
-    }
-    const handleClick =() =>{
-        setShow(500)
+    const [clicked, setClicked] = useState(true);
+    const [answer, setAnswer] = useState(2);
+    const handleClick = () => {
+        setClicked(!clicked);
+        if (clicked) {
+            // setShow(qnas.answer.length);
+            setAnswer(qnas.length)
+        }
+        else {
+            // setShow(150);
+            setAnswer(2)
+        }
     }
     return (
         <div className="card hover:shadow-2xl md:w-auto bg-base-100 shadow-xl hover:bg-[]">
@@ -16,13 +21,15 @@ const Qna = ({ qnas }) => {
                 <h2 className="card-title text-3xl font-extrabold">Frequently Asked Questions!</h2>
                 <hr />
                 {
-                    qnas.map(qna => <div key = {qna.id}><div className="p-4 border rounded-md mb-4">
-                            <h2 className="text-xl card-title font-bold mb-2">{qna.question}</h2>
-                            <p className="text-gray-700">{qna.answer.slice(0, show)}{(show <= 150) && <span>...</span>}</p>
-                        </div></div>)
+                    qnas.slice(0, answer).map(qna => <div key={qna.id}><div className="p-4 border rounded-md mb-4">
+                        <h2 className="text-xl card-title font-bold mb-2">{qna.question}</h2>
+                        <p className="text-gray-700">{}{qna.answer.slice(0, clicked ? 150 : qna.answer.length)}{clicked && <span>...</span>}</p>
+                    </div></div>)
                 }
                 <div className="card-actions justify-end">
-                    <button className="btn nav-clickable text-white" onClick={handleClick}>See More</button>
+                    <button className="btn nav-clickable text-white" onClick={handleClick}>{
+                        clicked ? "See More" : "See Less"
+                    }</button>
                 </div>
             </div>
         </div>
