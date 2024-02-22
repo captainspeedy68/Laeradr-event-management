ab8264facef41fca87d5648b6c6d63f5efed6c90
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-02-23 01:28:42 +0600

    chnges in the marquee on register or login

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -19,7 +19,10 @@ const Home = () => {
         <div className='m-0 p-0'>
             <Banner services={services}></Banner>
             <Marquee speed={80} className='relative top-72 my-10'>
-                <p className='text-md font-medium text-[#878787] mx-5 mt-10'><span className='font-extrabold'>Breaking News: </span>30% Off In June~July For All Tech Events</p>
+                {user ? <p className='text-md font-bold text-[#878787] mx-5 mt-10'><span className='font-extrabold'>Breaking News: </span>30% Off In June~July For All Tech Events</p>
+                    :
+                    <p className='text-lg font-bold text-[#878787] mx-5 mt-10'><span className='font-extrabold'>Notice: </span> Login / Register to see more features!</p>
+                }
             </Marquee>
             <hr className='relative top-80 my-10 border-1 border-[#fdac2e]' />
             <div className='lg:grid grid-cols-2 mx-auto my-10'>
@@ -27,7 +30,7 @@ const Home = () => {
                 <Services services={services}></Services>
             </div>
             <hr className='border-1 border-[#FDAC2E]' />
-            <Messages className = "mx-auto"></Messages>
+            <Messages className="mx-auto"></Messages>
         </div>
     );
 };
