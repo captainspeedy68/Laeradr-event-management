6f688661f60b0292eb953d929659d11b9ce9f67b
captainspeedy68 <bishwajitacharya303@gmail.com>
2024-02-23 00:56:07 +0600

    adding the photos of events to the gallery

--- src/pages/Gallery/Gallery.jsx
+++ src/pages/Gallery/Gallery.jsx
@@ -5,17 +5,31 @@ import GalleryList from './GalleryList';
 
 const Gallery = () => {
     const [services, setServices] = useState([]);
+    const [events, setEvents] = useState([])
     useEffect(() => {
         fetch("/services.json")
             .then(res => res.json())
             .then(data => setServices(data))
-    }, [])
+    }, []);
+    useEffect(() => {
+        fetch("/events.json")
+            .then(res => res.json())
+            .then(data => setEvents(data))
+    }, []);
+
     return (
         <div className="carousel carousel-center  p-4 space-x-4 bg-neutral rounded-box">
             {
                 services.map(service =>
                     <div className="carousel-item">
                         <img src={service.image_url} className="rounded-box w-96" />
+
+                    </div>)
+            }
+            {
+                events.map(event =>
+                    <div className="carousel-item">
+                        <img src={event.image_url} className="rounded-box w-96" />
 
                     </div>)
             }
